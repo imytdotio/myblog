b167b22c5cd20676d5016f0ddc0019bf345a65f8
imytdotio <imytdotio@gmail.com>
2022-10-21 13:21:51 +0800

    substring and dangerouslySetINnerHTML

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -6,11 +6,19 @@ import React from "react";
  **/
 
 const Product = (props) => {
+  const description = `${props.description.substring(0, 99)} ...`;
   return (
     <div className="border-2 border-gray-600 rounded-md m-2 p-2 w-5/12 inline-block">
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
-      <a class="gumroad-button" href={props.href} data-gumroad-overlay-checkout="true">
+      <h1 className="mb-2 font-bold">{props.title}</h1>
+      <div
+        dangerouslySetInnerHTML={{ __html: description }}
+        className="text-left"
+      />
+      <a
+        class="gumroad-button"
+        href={props.href}
+        data-gumroad-overlay-checkout="true"
+      >
         Buy on
       </a>
     </div>
