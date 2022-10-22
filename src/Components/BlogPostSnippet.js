da0c97bb6dc79e62424facba07c983ac506977ed
imytdotio <imytdotio@gmail.com>
2022-10-22 15:30:03 +0800

    nothing had changed

--- src/Components/BlogPostSnippet.js
+++ src/Components/BlogPostSnippet.js
@@ -6,30 +6,11 @@ import React from "react";
  **/
 
 const BlogPostSnippet = (props) => {
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
   return (
     <div className="grid grid-cols-6 bg-white shadow-xl my-4 mx-2 p-4 py-6">
       <div className="col-span-1">
-        <p className="text-4xl font-black">
-          {new Date(props.date).getDate()}
-        </p>
-        <p className="text-xl">
-          {monthNames[new Date(props.date).getMonth()]}
-        </p>
+        <p className="text-4xl font-black">{new Date(props.date).getDate()}</p>
+        <p className="text-xl">{monthNames[new Date(props.date).getMonth()]}</p>
       </div>
       <div className="col-span-5 text-left">
         <h1 className="text-xl font-black mb-4">{props.title}</h1>
@@ -39,3 +20,18 @@ const BlogPostSnippet = (props) => {
   );
 };
 export default BlogPostSnippet;
+
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
